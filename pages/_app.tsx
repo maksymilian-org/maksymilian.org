cf1975b715eea173cb3f05da4b2cd5730794df1a
Maksymilian Furtak <maksymilian.org@gmail.com>
2022-10-29 18:46:35 +0200

    Update head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "../components/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  );
 }
